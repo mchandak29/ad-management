0a11d8d417d03d7500cbab68f77eb5a5e0f92a32
raghuwan <raghuwan@adobe.com>
2023-01-12 16:00:30 +0530

    Integrating service API and fixing video issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -86,6 +86,7 @@ async function addLoopingContent(main, apiResponse) {
   const ads = apiResponse.data;
   const carousel = document.createElement('div');
   carousel.className = 'carousel slide';
+  carousel.id='colesdemo';
   // carousel.setAttribute('data-ride', 'carousel');
   carousel.setAttribute('data-interval', '15000');
   carousel.setAttribute('data-keyboard', 'false');
@@ -98,15 +99,19 @@ async function addLoopingContent(main, apiResponse) {
   for (let i = 0; i < ads.length; i++) {
     const carouselItem = document.createElement('div');
     carouselItem.className = (i === 0) ? 'carousel-item active' : 'carousel-item';
+    carouselItem.setAttribute('data-interval', (ads[i]['Duration'] * 1000).toString());
 
     const isVideo = ads[i]['Published Link'].endsWith('.mp4');
     if (isVideo) {
       const carouselVideo = document.createElement('video');
       carouselVideo.setAttribute('src', ads[i]['Published Link']);
-      carouselItem.appendChild(carouselVideo);
       carouselVideo.className = 'd-flex w-100';
-      carouselVideo.setAttribute('autoplay', 'autoplay');
-      carouselVideo.setAttribute('loop', 'loop');
+      let carouselVideoSource = document.createElement('source');
+      carouselVideoSource.setAttribute('src', ads[i]['Published Link']);
+      carouselVideoSource.setAttribute('type', 'video/mp4');
+      //carouselVideo.setAttribute('autoplay', 'autoplay');
+      //carouselVideo.setAttribute('loop', 'loop');
+      carouselVideo.appendChild(carouselVideoSource);
       carouselItem.appendChild(carouselVideo);
     } else {
       const carouselImage = createOptimizedPicture(ads[i]['Published Link'], '', true);
@@ -118,6 +123,32 @@ async function addLoopingContent(main, apiResponse) {
   carousel.appendChild(carouselInner);
   main.replaceChild(carousel, document.querySelector('div'));
   $('.carousel').carousel();
+  carouselInterval();
+  videoPlay();
+}
+
+function videoPlay() {
+  $('#colesdemo').on('slid.bs.carousel', function () {
+    if($('#colesdemo').find('.active').children()[0].nodeName=='VIDEO'){
+      let vid = $('#colesdemo').find('.active').children()[0];
+      vid.muted = true;
+      vid.play();
+    }
+  })
+}
+
+function carouselInterval() {
+  let t;
+  let start = $('#colesdemo').find('.active').attr('data-interval');
+  t = setTimeout("$('#colesdemo').carousel({interval: 1000});", start-1000);
+
+  $('#colesdemo').on('slid.bs.carousel', function () {
+    clearTimeout(t);
+    var duration = $(this).find('.active').attr('data-interval');
+
+    $('#colesdemo').carousel('pause');
+    t = setTimeout("$('#colesdemo').carousel();", duration-1000);
+  })
 }
 
 async function pollAPI(main, fn, url, interval, previousResponse) {
@@ -144,7 +175,7 @@ const poll = (main, fn, url) => {
   }
   if (!url) {
     // eslint-disable-next-line no-param-reassign
-    url = 'https://main--screens-ad-management--mchandak29.hlx.page/final-playlist.json';
+    url = 'https://288650-franklinecolesdemo-stage.adobeio-static.net/api/v1/web/franklin_coles_demo/playlist';
   }
   console.log('Start poll...');
   setTimeout(() => {
