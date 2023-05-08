4414965c5f7bb1a09d0a58d39c31ecae22440be3
Vivek Singh <38683124+vivesing@users.noreply.github.com>
2023-05-08 19:41:48 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -175,6 +175,7 @@ const poll = (main, fn, url) => {
   }
   if (!url) {
     // eslint-disable-next-line no-param-reassign
+    // test change
     // url = 'https://288650-franklinecolesdemo-stage.adobeio-static.net/api/v1/web/franklin_coles_demo/playlist';
     url = 'https://288650-257ambermackerel.adobeio-static.net/api/v1/web/colesdemoapi/urlprovider';
   }
